d94663fb015f5aab3023bcc7eaeda08d0e0a5289
BloodyRnar <lafekafe@hotmail.com>
2021-10-21 22:49:22 -0500

    V1.1.3

--- data.lua
+++ data.lua
@@ -1,18 +1,4 @@
 data:extend({
-
-	{
-    type = "fluid",
-    name = "water-sodium-hydroxide-solution",
-    icon = "__seablocksodiumhydroxidesolution__/graphics/icons/water-sodium-hydroxide-solution.png",
-	icon_size = 32,
-	default_temperature = 100,
-    heat_capacity = "0.2KJ",
-    base_color = {r = 1, g = 1, b = 1},
-    flow_color = {r = 1, g = 1, b = 1},
-    max_temperature = 1000,
-	pressure_to_speed_ratio = 0.4,
-    flow_to_energy_ratio = 0.59,
-	},
 	
 	{
     type = "technology",
@@ -27,7 +13,7 @@ data:extend({
     {
 	  {
         type = "unlock-recipe",
-        recipe = "solid-sodium-hydroxide-water-sodium-hydroxide-solution"
+        recipe = "liquid-aqueous-sodium-hydroxide"
       },
     },
     unit =
@@ -42,46 +28,6 @@ data:extend({
     order = "c-a"
     },
 
-    {
-    type = "recipe",
-    name = "solid-sodium-hydroxide-water-sodium-hydroxide-solution",
-    category = "chemistry",
-	subgroup = "petrochem-sodium",
-    energy_required = 1,
-	enabled = "true",
-    ingredients ={
-		{type="item", name="solid-sodium-hydroxide", amount=1},
-		{type="fluid", name="water-purified", amount=50},
-	},
-    results=
-    {
-		{type="fluid", name="water-sodium-hydroxide-solution", amount=50},
-    },
-    icon = "__seablocksodiumhydroxidesolution__/graphics/icons/water-sodium-hydroxide-solution.png",
-	icon_size = 32,
-	},
-	
-	
-	{
-	type = "recipe",
-	name = "angels-water-void-water-sodium-hydroxide-solution",
-	category = "angels-water-void",
-	enabled = "true",
-	hidden = "true",
-	energy_required = 5,
-	ingredients =
-	{
-	  {type= "fluid", name="water-sodium-hydroxide-solution", amount=400}
-	},
-	results=
-	{
-	  {type="item", name="water-void", amount=1, probability=0},
-	},
-	subgroup = "angels-void",
-	icon = "__seablocksodiumhydroxidesolution__/graphics/icons/water-sodium-hydroxide-solution.png",
-	icon_size = 32,
-	order = "angels-water-void-water-sodium-hydroxide-solution",
-	},
 	
 })
   
